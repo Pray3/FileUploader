0f71675297f1302b99858eeb2a80f7bf2c8aa778
Pray3 <735250105@qq.com>
2020-08-13 23:40:24 +0800

    httpDecoder

--- FileUploader/src/main/java/handler/FileUploadHandler.java
+++ FileUploader/src/main/java/handler/FileUploadHandler.java
@@ -2,12 +2,20 @@ package handler;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpUtil;
 
 import java.util.logging.FileHandler;
 
-public class FileUploadHandler extends SimpleChannelUpstreamHandler{
+public class FileUploadHandler extends ChannelInboundHandlerAdapter{
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        if(msg instanceof HttpRequest){
+            //因为在前面已经做了netty封装好的httpEncoder的流程，这里的对象就被转换成HttpRequest对象了
+            HttpRequest request = (HttpRequest) msg;
+            boolean keepAlive = HttpUtil.isKeepAlive(request);
+            long contentLength = HttpUtil.getContentLength(request);
+        }
         super.channelRead(ctx, msg);
     }
 
