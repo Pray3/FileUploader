a5ee253cb99ec3d4bc73206b71b221a5ede94c0f
cgj-com <735250105@qq.com>
2020-08-14 17:24:08 +0800

    updete

--- FileUploader/src/main/java/server/FileUploadServer.java
+++ FileUploader/src/main/java/server/FileUploadServer.java
@@ -8,8 +8,10 @@ import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpRequestDecoder;
 import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.codec.http.HttpServerExpectContinueHandler;
 
 public class FileUploadServer {
 
@@ -23,7 +25,11 @@ public class FileUploadServer {
                 .childHandler(new ChannelInitializer<SocketChannel>() {
                     protected void initChannel(SocketChannel ch) throws Exception {
                         //解析http请求
-                        ch.pipeline().addLast(new HttpRequestDecoder() , new FileUploadHandler(),new HttpResponseEncoder());
+                        ch.pipeline().addLast(new HttpRequestDecoder() ,
+                                new HttpObjectAggregator(1024*1024),//处理post请求
+                                new HttpServerExpectContinueHandler(),//POST大数据处理
+                                new FileUploadHandler(),
+                                new HttpResponseEncoder());
                     }
                 })
                 .option(ChannelOption.SO_BACKLOG,128)
