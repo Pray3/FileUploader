1040093e6e3a9c4811e91829890784956b742766
Pray3 <735250105@qq.com>
2020-08-16 22:55:08 +0800

    update

--- FileUploader/src/main/java/handler/FileUploadHandler.java
+++ FileUploader/src/main/java/handler/FileUploadHandler.java
@@ -1,11 +1,20 @@
 package handler;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
 
+import org.apache.commons.codec.CharEncoding;
+import org.apache.commons.codec.Charsets;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 import java.util.logging.FileHandler;
+
 
 public class FileUploadHandler extends SimpleChannelInboundHandler<HttpObject> {
 
@@ -17,13 +26,63 @@ public class FileUploadHandler extends SimpleChannelInboundHandler<HttpObject> {
         if (msg instanceof HttpRequest){
             request = (HttpRequest)msg;
             headers = request.headers();
-
+            String uri = request.uri();
             HttpMethod method = request.method();
             if(method.equals(HttpMethod.GET)){
-
+                QueryStringDecoder queryStringDecoder = new QueryStringDecoder(uri, Charsets.toCharset(CharEncoding.UTF_8));
+                Map<String, List<String>> parameters = queryStringDecoder.parameters();
+                //此处仅打印请求参数（你可以根据业务需求自定义处理）
+                for (Map.Entry<String, List<String>> attr : parameters.entrySet()) {
+                    for (String attrVal : attr.getValue()) {
+                        System.out.println((attr.getKey() + "=" + attrVal));
+                    }
+                }
             }else if (method.equals(HttpMethod.POST)){
-
+                //POST请求,由于你需要从消息体中获取数据,因此有必要把msg转换成FullHttpRequest
+                fullRequest = (FullHttpRequest)msg;
+                //根据不同的Content_Type处理body数据
+                dealWithContentType();
             }
         }
+
     }
+
+
+    /**
+     * 简单处理常用几种 Content-Type 的 POST 内容（可自行扩展）
+     * @throws Exception
+     */
+    private void dealWithContentType() throws Exception{
+        String contentType = getContentType();
+        if(contentType.equals("application/json")){
+            String jsonStr = fullRequest.content().toString(Charsets.toCharset(CharEncoding.UTF_8));
+            JSONObject obj = JSON.parseObject(jsonStr);
+            for(Map.Entry<String, Object> item : obj.entrySet()){
+                System.out.println(item.getKey()+"="+item.getValue().toString());
+            }
+
+        }else if(contentType.equals("application/x-www-form-urlencoded")){
+            //方式一：使用 QueryStringDecoder
+            String jsonStr = fullRequest.content().toString(Charsets.toCharset(CharEncoding.UTF_8));
+            QueryStringDecoder queryDecoder = new QueryStringDecoder(jsonStr, false);
+            Map<String, List<String>> uriAttributes = queryDecoder.parameters();
+            for (Map.Entry<String, List<String>> attr : uriAttributes.entrySet()) {
+                for (String attrVal : attr.getValue()) {
+                    System.out.println(attr.getKey() + "=" + attrVal);
+                }
+            }
+
+        }else if(contentType.equals("multipart/form-data")){
+            //TODO 用于文件上传
+        }else{
+            //do nothing...
+        }
+    }
+
+    private String getContentType() {
+        String typeStr = headers.get("Content-Type").toString();
+        String[] list = typeStr.split(";");
+        return list[0];
+    }
+
 }
